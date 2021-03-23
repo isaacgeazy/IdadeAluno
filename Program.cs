a9c05b1a66d679f9f7fc1be3f0c4db7ac89204a6
Isaac <isaacgeazy@gmail.com>
2021-03-23 14:13:17 -0300

    Atividade Idade do Aluno

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IdadeAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime aniversario = new DateTime(2003, 01, 10);
+            DateTime anv = new DateTime(2021, 01, 10);
+            Console.WriteLine($"Eu nasci numa {aniversario.ToLongDateString()}");
+            Console.WriteLine($"Meu aniversário de 18 foi em um {anv.ToLongDateString()}");
+
+        }
+    }
+}
